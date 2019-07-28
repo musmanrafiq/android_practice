dc3dc72f4fdaec3763afaf5a0b594d8468440065
usman <usman.solemn@gmail.com>
2019-07-28 11:08:18 +0500

    Framelayout and Bundle vs Intent practice.

---
+++ app/src/main/java/com/dev/neo/androidlearning/FrameLayoutActivity.java
@@ -0,0 +1,48 @@
+package com.dev.neo.androidlearning;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class FrameLayoutActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    private ImageView i1,i2,i3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+
+        i1 = findViewById(R.id.image1);
+        i2 = findViewById(R.id.image2);
+        i3 = findViewById(R.id.image3);
+
+        i1.setOnClickListener(this);
+        i2.setOnClickListener(this);
+        i3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.image1){
+            i1.setVisibility(View.GONE);
+            i2.setVisibility(View.VISIBLE);
+            i3.setVisibility(View.GONE);
+        }
+        else if(v.getId() == R.id.image2){
+            i1.setVisibility(View.GONE);
+            i2.setVisibility(View.GONE);
+            i3.setVisibility(View.VISIBLE);
+        }
+        else if(v.getId() == R.id.image3){
+            i1.setVisibility(View.VISIBLE);
+            i2.setVisibility(View.GONE);
+            i3.setVisibility(View.GONE);
+        }
+    }
+}
+
--- app/src/main/java/com/dev/neo/androidlearning/MainActivity.java
+++ app/src/main/java/com/dev/neo/androidlearning/MainActivity.java
@@ -1,7 +1,8 @@
 package com.dev.neo.androidlearning;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        //i.putExtra("myData", "This is value");
+
+        Bundle b = new Bundle();
+        b.putString("myData", "This is value");
+
+        i.putExtras(b);
+        startActivity(i);
     }
 }
