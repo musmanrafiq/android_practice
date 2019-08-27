7e931fe237d4028f364fcab417a2c33d4d0bf310
usman <usman.solemn@gmail.com>
2019-08-27 23:18:27 +0500

    Spinner with string adapter

--- app/src/main/java/com/dev/neo/androidlearning/activities/SpinnerActivity.java
+++ app/src/main/java/com/dev/neo/androidlearning/activities/SpinnerActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.dev.neo.androidlearning.R;
+
+import java.util.ArrayList;
 
 public class SpinnerActivity extends AppCompatActivity {
 
@@ -20,6 +23,15 @@ public class SpinnerActivity extends AppCompatActivity {
 
         // initialization
         employTypeSpinner = findViewById(R.id.employType);
+
+        ArrayList<String> values = new ArrayList<String>();
+        values.add("Manager");
+        values.add("Accountant");
+        values.add("Guard");
+
+        ArrayAdapter<String> spinerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, values);
+        employTypeSpinner.setAdapter(spinerAdapter);
+
         employTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
